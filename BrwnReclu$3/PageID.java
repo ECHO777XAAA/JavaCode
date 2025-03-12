4dce4d4fa2e0f6df3bb290db8eed305bb3d880af
ECHO777XAAA <christianortizblanco26@hotmail.com>
2025-03-12 12:38:57 -0400

    Update PageID.java

--- BrwnReclu$3/PageID.java
+++ BrwnReclu$3/PageID.java
@@ -3,6 +3,9 @@
     Page handler for the BrownRecluse Program
 */
 
+/*
+  Class is passed a URL and test status (UP, DOWN, Error) Returns Status (Appeneded to url in print class)  
+*/
 
 Package BrwnReclu$3;
 
