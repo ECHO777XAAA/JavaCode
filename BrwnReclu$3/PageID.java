f78128995ef9abfd32030d78416f4ae3c15f4b65
ECHO777XAAA <christianortizblanco26@hotmail.com>
2025-03-15 12:00:05 -0400

    Update PageID.java

--- BrwnReclu$3/PageID.java
+++ BrwnReclu$3/PageID.java
@@ -10,5 +10,13 @@
 Package BrwnReclu$3;
 
 public class PageID{
-
+			
+			public static String Crawler(//INPUT HTML FILE){
+				//this method takes an html file and outputs each word
+				//method counts the number of words in the file
+			}
+			
+			public static ArrayList<String> URLparse(String Word,int count){
+				//this method takes a word and concatanates them into an arraylist 
+			}
 }
