8fbd5f22157985c72c4e650dfa5456ef690dc1e3
ECHO777XAAA <christianortizblanco26@hotmail.com>
2025-03-15 13:18:35 -0400

    Update Database.java

--- BrwnReclu$3/Database.java
+++ BrwnReclu$3/Database.java
@@ -10,13 +10,19 @@ package BrownRelu$3;
 
 public class Database extends BrownRecluse{
     
-	private HashMap<String, ArrayList<String>> ArrayList;
+	private HashMap<String, ArrayList<String>> DataB;
 
 	public DBase(){
-			ArrayList = new HashMap<>();
+				DataB = new HashMap<>();
 	}
 	Public void addArrayList(Integer Index,ArrayList<String> arrayList){
-
+				DataB.put(Index,arrayList);
+	}
+	public ArrayList<String> getArrayList(Integer Index){
+				return DataB.get(Index);	
+	}
+	public boolean containsArrayList(Integer Index){
+				return DataB.containsKey(Index);
 	}
 	//this class stores each array list as an object to be parsed through later
   
