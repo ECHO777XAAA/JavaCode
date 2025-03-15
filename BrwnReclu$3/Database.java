ba10ae9f3723c5e6ac5be96c16e1b3df05475ba7
ECHO777XAAA <christianortizblanco26@hotmail.com>
2025-03-15 12:02:51 -0400

    Update Database.java

--- BrwnReclu$3/Database.java
+++ BrwnReclu$3/Database.java
@@ -7,6 +7,8 @@ package BrownRelu$3;
 
 public class Database extends BrownRecluse{
     
-  ArrayList<String> DB1= new ArrayList<String>();
+	ArrayList<String> DB1= new ArrayList<String>();
+	
+	//this class stores each array list as an object to be parsed through later
   
 }
